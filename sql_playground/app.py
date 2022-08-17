9e792212cd374f19df72a1ce140463ee50749a6e
minyeamer <minyeamer@gmail.com>
2022-08-17 23:04:23 +0900

    set page config and dataframe style

--- sql_playground/app.py
+++ sql_playground/app.py
@@ -1,23 +1,26 @@
-import streamlit as st 
-import pandas as pd 
+import streamlit as st
+import pandas as pd
+import webbrowser
 
 # DB Mgmt
-import sqlite3 
+import sqlite3
 conn = sqlite3.connect("data/world.sqlite", check_same_thread=False)
 cursor = conn.cursor()
 
 def sql_executor(raw_code):
     cursor.execute(raw_code)
     data = cursor.fetchall()
-    return data
+    columns = [desc[0] for desc in cursor.description]
+    return data, columns
 
 city = ['ID,', 'Name,', 'CountryCode,', 'District,', 'Population']
 country = ['Code,', 'Name,', 'Continent,', 'Region,', 'SurfaceArea,', 'IndepYear,', 'Population,', 'LifeExpectancy,', 'GNP,', 'GNPOld,', 'LocalName,', 'GovernmentForm,', 'HeadOfState,', 'Capital,', 'Code2']
 countrylanguage = ['CountryCode,', 'Language,', 'IsOfficial,', 'Percentage']
 
 def main():
+    st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
     st.title("SQLPlayground")
-    
+
     menu = ["Home", "About"]
     choice = st.sidebar.selectbox("Menu", menu)
     
@@ -35,24 +38,30 @@ def main():
             with st.expander("Table Info"):
                 t_info = {'city' : city, 'country' : country, 'countrylanguage' : countrylanguage}
                 st.json(t_info)
-                
+
         # Result Layouts
         with col2:
             if submit_code:
                 st.info("Query Submitted")
                 st.code(raw_code)
-                
+
                 # Results 
-                query_results = sql_executor(raw_code)
+                query_results, columns = sql_executor(raw_code)
                 with st.expander("Results"):
                     st.write(query_results)
-                    
+
                 with st.expander("Pretty Table"):
-                    query_df = pd.DataFrame(query_results)
-                    st.dataframe(query_df)
+                    query_df = pd.DataFrame(query_results, columns=columns)
+                    st._legacy_dataframe(query_df)
+            else:
+                st.info("Query Required")
 
     elif choice == "About":
-        st.subheader("About")
+        st.subheader("Reference")
+        if st.button("Github link"):
+            webbrowser.open_new_tab("https://github.com/minyeamer/streamlit")
+        if st.button("Original source"):
+            webbrowser.open_new_tab("https://github.com/Jcharis/Streamlit_DataScience_Apps")
 
 if __name__ == "__main__":
     main()
